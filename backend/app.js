5ea3db343a2a9e180298501bfe213619f37bd823
Sergey Klopov <sergklo94@gmail.com>
2023-05-29 22:04:58 +0300

    убрать краш тест

--- backend/app.js
+++ backend/app.js
@@ -36,12 +36,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.post('/signin', loginValidation, login);
 app.post('/signup', createUserValidation, createUser);
 app.get('/signout', logout);
